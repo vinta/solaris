efa0b7bd571ad4ca14a3c3369999ccf3883d54d7
Vinta Chen <vinta.chen@gmail.com>
2023-11-17 10:00:51 +0800

    add mixins

--- contracts/arbitrage/Arbitrageur.sol
+++ contracts/arbitrage/Arbitrageur.sol
@@ -5,19 +5,11 @@ import { Ownable } from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import { SafeERC20 } from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
-import { IUniswapV3Router } from "./interfaces/IUniswapV3Router.sol";
-import { IVelodromeV2Router } from "./interfaces/IVelodromeV2Router.sol";
+import { UniswapV3Mixin } from "./mixins/UniswapV3Mixin.sol";
+import { VelodromeV2Mixin } from "./mixins/VelodromeV2Mixin.sol";
 
-contract Arbitrageur is Ownable {
+contract Arbitrageur is Ownable, VelodromeV2Mixin, UniswapV3Mixin {
     using SafeERC20 for IERC20;
-
-    // https://docs.uniswap.org/contracts/v3/reference/deployments
-    // https://github.com/Uniswap/swap-router-contracts/blob/main/contracts/SwapRouter02.sol
-    address public constant UNISWAP_V3_SWAP_ROUTER_02 = address(0x2626664c2603336E57B271c5C0b26F421741e481);
-    // https://aerodrome.finance/security#contracts
-    // https://github.com/velodrome-finance/contracts/blob/main/contracts/Router.sol
-    address public constant VELODROME_V2_ROUTER = address(0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43);
-    address public constant VELODROME_V2_POOL_FACTORY = address(0x420DD381b31aEf6683db6B902084cB0FFECe40Da);
 
     error NoProfit();
 
@@ -25,26 +17,6 @@ contract Arbitrageur is Ownable {
 
     function withdrawAll(address token) external onlyOwner {
         IERC20(token).safeTransfer(owner(), IERC20(token).balanceOf(address(this)));
-    }
-
-    function arbitrageUniswapV3toVelodromeV2(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        uint256 minProfit,
-        uint24 uniswapV3Fee,
-        bool velodromeV2Stable
-    ) external {
-        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
-
-        uint256 amountOutFromFirst = _swapOnUniswapV3(tokenIn, tokenOut, amountIn, uniswapV3Fee);
-        uint256 amountOut = _swapOnVelodromeV2(tokenOut, tokenIn, amountOutFromFirst, velodromeV2Stable);
-
-        if (amountOut <= amountIn + minProfit) {
-            revert NoProfit();
-        }
-
-        IERC20(tokenIn).safeTransfer(msg.sender, amountOut);
     }
 
     function arbitrageVelodromeV2toUniswapV3(
@@ -67,53 +39,23 @@ contract Arbitrageur is Ownable {
         IERC20(tokenIn).safeTransfer(msg.sender, amountOut);
     }
 
-    // internal
-
-    function _swapOnUniswapV3(
+    function arbitrageUniswapV3toVelodromeV2(
         address tokenIn,
         address tokenOut,
         uint256 amountIn,
-        uint24 fee
-    ) internal returns (uint256) {
-        IERC20(tokenIn).approve(UNISWAP_V3_SWAP_ROUTER_02, amountIn);
+        uint256 minProfit,
+        uint24 uniswapV3Fee,
+        bool velodromeV2Stable
+    ) external {
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
 
-        return
-            IUniswapV3Router(UNISWAP_V3_SWAP_ROUTER_02).exactInputSingle(
-                IUniswapV3Router.ExactInputSingleParams({
-                    tokenIn: tokenIn,
-                    tokenOut: tokenOut,
-                    fee: fee,
-                    recipient: address(this),
-                    amountIn: amountIn,
-                    amountOutMinimum: 0,
-                    sqrtPriceLimitX96: 0
-                })
-            );
-    }
+        uint256 amountOutFromFirst = _swapOnUniswapV3(tokenIn, tokenOut, amountIn, uniswapV3Fee);
+        uint256 amountOut = _swapOnVelodromeV2(tokenOut, tokenIn, amountOutFromFirst, velodromeV2Stable);
 
-    function _swapOnVelodromeV2(
-        address tokenIn,
-        address tokenOut,
-        uint256 amountIn,
-        bool stable
-    ) internal returns (uint256) {
-        IERC20(tokenIn).approve(VELODROME_V2_ROUTER, amountIn);
+        if (amountOut <= amountIn + minProfit) {
+            revert NoProfit();
+        }
 
-        IVelodromeV2Router.Route[] memory routes = new IVelodromeV2Router.Route[](1);
-        routes[0] = IVelodromeV2Router.Route({
-            from: tokenIn,
-            to: tokenOut,
-            stable: stable,
-            factory: VELODROME_V2_POOL_FACTORY
-        });
-        uint256[] memory amounts = IVelodromeV2Router(VELODROME_V2_ROUTER).swapExactTokensForTokens(
-            amountIn,
-            0,
-            routes,
-            address(this),
-            block.timestamp
-        );
-
-        return amounts[amounts.length - 1];
+        IERC20(tokenIn).safeTransfer(msg.sender, amountOut);
     }
 }
---
+++ contracts/arbitrage/mixins/UniswapV3Mixin.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
+import { IUniswapV3Router } from "../interfaces/IUniswapV3Router.sol";
+
+abstract contract UniswapV3Mixin {
+    // https://docs.uniswap.org/contracts/v3/reference/deployments
+    // https://github.com/Uniswap/swap-router-contracts/blob/main/contracts/SwapRouter02.sol
+    address public constant UNISWAP_V3_SWAP_ROUTER_02 = address(0x2626664c2603336E57B271c5C0b26F421741e481);
+
+    // internal
+
+    function _swapOnUniswapV3(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint24 fee
+    ) internal returns (uint256) {
+        IERC20(tokenIn).approve(UNISWAP_V3_SWAP_ROUTER_02, amountIn);
+
+        return
+            IUniswapV3Router(UNISWAP_V3_SWAP_ROUTER_02).exactInputSingle(
+                IUniswapV3Router.ExactInputSingleParams({
+                    tokenIn: tokenIn,
+                    tokenOut: tokenOut,
+                    fee: fee,
+                    recipient: address(this),
+                    amountIn: amountIn,
+                    amountOutMinimum: 0,
+                    sqrtPriceLimitX96: 0
+                })
+            );
+    }
+}
