f0fb3be71cc35e81ce06095050cd49fa4f6caf8b
Vinta Chen <vinta.chen@gmail.com>
2023-11-17 22:12:55 +0800

    exclude BaseTest from coverage report

--- tests/BaseTest.sol
+++ tests/BaseTest.sol
@@ -4,13 +4,7 @@ pragma solidity 0.8.19;
 import "forge-std/Test.sol";
 
 contract BaseTest is Test {
-    // function test_excludeFromCoverageReport() public virtual {
-    //     // workaround: https://github.com/foundry-rs/foundry/issues/2988#issuecomment-1437784542
-    // }
-
-    function _enableInitialize(address account) internal {
-        // We assume Initializable is always at the top of the inheritance chain, so `_initialized` is always in slot 0.
-        bytes32 _slotInitialized = bytes32(uint256(0));
-        vm.store(account, _slotInitialized, bytes32(uint256(0)));
+    function test_excludeFromCoverageReport() public virtual {
+        // workaround: https://github.com/foundry-rs/foundry/issues/2988#issuecomment-1437784542
     }
 }
