f578173909888f1d7af4611aa3783bfca582e48e
Vinta Chen <vinta.chen@gmail.com>
2023-12-01 02:19:52 +0800

    1inch arbitrage

---
+++ packages/flash-aggregate-arbitrageur/src/arbitrageur.ts
@@ -0,0 +1,169 @@
+import { Handler } from "aws-lambda"
+
+import { BaseArbitrageur } from "@solaris/common/src/base-arbitrageur"
+import { randomNumber, sleep, wrapSentryHandlerIfNeeded } from "@solaris/common/src/utils"
+
+import { getRandomIntentions, Intention, oneInchProtocols } from "./configs"
+import { FlashAggregateArbitrageur, FlashAggregateArbitrageur__factory } from "../types"
+
+class FlashAggregateArbitrageurOnOptimism extends BaseArbitrageur {
+    ONEINCH_API_ENDPOINT = process.env.ONEINCH_API_ENDPOINT!
+    ONEINCH_API_KEY = process.env.ONEINCH_API_KEY!
+
+    arbitrageur!: FlashAggregateArbitrageur
+
+    // UniswapV3Router
+    ERROR_TOO_LITTLE_RECEIVED = "Too little received"
+
+    // OneInchRouterV5
+    ERROR_ONEINCH_SWAP_FAIL = "OneInchSwapFail"
+
+    async start() {
+        const startTimestamp = Date.now() / 1000
+
+        const network = this.getNetwork()
+        const provider = this.getProvider(this.RPC_PROVIDER_URL, network, {})
+        const sequencerProvider = this.getProvider(this.SEQUENCER_RPC_PROVIDER_URL, network, {})
+
+        this.owner = await this.getOwner(provider)
+        this.ownerWithSequencerProvider = this.owner.connect(sequencerProvider)
+        this.arbitrageur = FlashAggregateArbitrageur__factory.connect(this.ARBITRAGEUR_ADDRESS, this.owner)
+
+        console.log("start", {
+            awsRegion: process.env.AWS_REGION,
+            rpcProviderUrl: this.RPC_PROVIDER_URL,
+            sequencerRpcProviderUrl: this.SEQUENCER_RPC_PROVIDER_URL,
+            arbitrageur: this.ARBITRAGEUR_ADDRESS,
+            owner: this.owner.address,
+        })
+
+        let i = 0
+        while (true) {
+            i++
+
+            const intentions = getRandomIntentions(6)
+
+            await Promise.all(intentions.map((intention) => this.tryArbitrage(intention)))
+
+            const nowTimestamp = Date.now() / 1000
+            if (nowTimestamp - startTimestamp >= this.TIMEOUT_SECONDS) {
+                console.log(`arbitrage end: ${i}`)
+                return
+            }
+        }
+    }
+
+    async tryArbitrage(intention: Intention) {
+        let oneInchData: string
+        try {
+            oneInchData = await this.fetchOneInchSwapData(intention)
+        } catch (err: any) {
+            const errMessage = err.message || err.reason || ""
+            if (errMessage.includes("TooManyRequests")) {
+                // console.log("Too Many Requests")
+                await sleep(1000 * randomNumber(0.2, 1))
+                return
+            }
+            throw err
+        }
+
+        try {
+            const profit = await this.arbitrageur.arbitrageOneInch.staticCall(
+                intention.tokenIn,
+                intention.tokenOut,
+                intention.amountIn,
+                intention.minProfit,
+                oneInchData,
+                intention.uniswapV3Fee,
+            )
+            await this.arbitrage(intention, profit, oneInchData)
+        } catch (err: any) {
+            const errMessage = err.message || err.reason || ""
+            if (
+                errMessage.includes(this.ERROR_TOO_LITTLE_RECEIVED) ||
+                errMessage.includes(this.ERROR_ONEINCH_SWAP_FAIL)
+            ) {
+                // console.log("No Profit")
+            } else {
+                throw err
+            }
+        }
+    }
+
+    async arbitrage(intention: Intention, profit: bigint, oneInchData: string) {
+        const gas = this.calculateGas(intention.tokenIn, profit)
+        const populateTx = await this.arbitrageur.arbitrageOneInch.populateTransaction(
+            intention.tokenIn,
+            intention.tokenOut,
+            intention.amountIn,
+            intention.minProfit,
+            oneInchData,
+            intention.uniswapV3Fee,
+        )
+
+        await this.sendTx(this.ownerWithSequencerProvider, async () => {
+            // NOTE: fill all required fields to avoid calling signer.populateTransaction(tx)
+            await this.ownerWithSequencerProvider.sendTransaction({
+                to: populateTx.to,
+                data: populateTx.data,
+                nonce: this.nonceManager.getNonce(this.ownerWithSequencerProvider),
+                gasLimit: this.GAS_LIMIT_PER_BLOCK,
+                chainId: this.NETWORK_CHAIN_ID,
+                type: gas.type,
+                maxFeePerGas: gas.maxFeePerGas,
+                maxPriorityFeePerGas: gas.maxPriorityFeePerGas,
+            })
+        })
+        console.log(`arbitrage tx sent, profit: ${profit} in ${intention.tokenIn}`)
+        process.exit(0)
+    }
+
+    async fetchOneInchSwapData(intention: Intention) {
+        const params = {
+            src: intention.tokenIn,
+            dst: intention.tokenOut,
+            amount: intention.amountIn.toString(),
+            from: this.ARBITRAGEUR_ADDRESS,
+            slippage: "50",
+            protocols: oneInchProtocols.join(","),
+            disableEstimate: "true",
+        }
+        const urlParams = new URLSearchParams(params)
+        const url = `${this.ONEINCH_API_ENDPOINT}/swap?${urlParams}`
+
+        const res = await fetch(url, {
+            method: "get",
+            headers: {
+                Accept: "application/json",
+                Authorization: `Bearer ${this.ONEINCH_API_KEY}`,
+            },
+        })
+
+        if (res.status === 429) {
+            throw new Error("FetchOneInchSwapData - TooManyRequests")
+        }
+
+        if (!res.ok) {
+            const err = new Error("FetchOneInchSwapData - Error")
+            err.message = await res.text()
+            throw err
+        }
+
+        const response = await res.json()
+
+        if (!response.tx?.data) {
+            const err = new Error("FetchOneInchSwapData - NoData")
+            err.message = await res.text()
+            throw err
+        }
+
+        return response.tx.data as string
+    }
+}
+
+const handler: Handler = async (event, context) => {
+    const service = new FlashAggregateArbitrageurOnOptimism()
+    await service.start()
+}
+
+export const optimism = wrapSentryHandlerIfNeeded(handler)
---
+++ packages/flash-aggregate-arbitrageur/src/configs.ts
@@ -0,0 +1,126 @@
+import { parseUnits } from "ethers"
+import { sampleSize } from "lodash"
+
+import { TOKENS } from "@solaris/common/src/constants"
+import { randomNumber } from "@solaris/common/src/utils"
+
+const multiplier = BigInt(10)
+
+export const minProfitMap = {
+    [TOKENS.USDC]: parseUnits("1", 6) * multiplier, // 1 USD
+    [TOKENS.USDCe]: parseUnits("1", 6) * multiplier, // 1 USD
+    [TOKENS.WETH]: parseUnits("0.0005", 18) * multiplier, // 1 USD
+    [TOKENS.OP]: parseUnits("1", 18) * multiplier, // 1 US
+    [TOKENS.PERP]: parseUnits("1", 18) * multiplier, // 1 USD
+    [TOKENS.SNX]: parseUnits("3", 18) * multiplier, // 1 USD
+}
+
+function getRandomAmount(min: number, max: number, decimals = 18, precision = 1) {
+    const amount = parseUnits(randomNumber(min, max, precision).toString(), decimals)
+    return amount
+}
+
+export interface Intention {
+    tokenIn: string
+    tokenOut: string
+    amountIn: bigint
+    minProfit: bigint
+    uniswapV3Fee: number
+}
+
+export function getRandomIntentions(size: number) {
+    const intentions: Intention[] = [
+        // FIXME: OneInchSwapFail()
+        // {
+        //     tokenIn: TOKENS.WETH,
+        //     tokenOut: TOKENS.USDC,
+        //     amountIn: getRandomAmount(1, 10, 18),
+        //     minProfit: minProfitMap[TOKENS.WETH],
+        //     uniswapV3Fee: 500,
+        // },
+        // {
+        //     tokenIn: TOKENS.USDC,
+        //     tokenOut: TOKENS.WETH,
+        //     amountIn: getRandomAmount(2000, 20000, 6),
+        //     minProfit: minProfitMap[TOKENS.USDC],
+        //     uniswapV3Fee: 500,
+        // },
+
+        {
+            tokenIn: TOKENS.WETH,
+            tokenOut: TOKENS.USDCe,
+            amountIn: getRandomAmount(1, 10, 18),
+            minProfit: minProfitMap[TOKENS.WETH],
+            uniswapV3Fee: 500,
+        },
+        {
+            tokenIn: TOKENS.USDCe,
+            tokenOut: TOKENS.WETH,
+            amountIn: getRandomAmount(2000, 20000, 6),
+            minProfit: minProfitMap[TOKENS.USDCe],
+            uniswapV3Fee: 500,
+        },
+        {
+            tokenIn: TOKENS.WETH,
+            tokenOut: TOKENS.OP,
+            amountIn: getRandomAmount(1, 10, 18),
+            minProfit: minProfitMap[TOKENS.WETH],
+            uniswapV3Fee: 3000,
+        },
+        {
+            tokenIn: TOKENS.OP,
+            tokenOut: TOKENS.WETH,
+            amountIn: getRandomAmount(1000, 2000, 18),
+            minProfit: minProfitMap[TOKENS.OP],
+            uniswapV3Fee: 3000,
+        },
+        {
+            tokenIn: TOKENS.WETH,
+            tokenOut: TOKENS.PERP,
+            amountIn: getRandomAmount(1, 10, 18),
+            minProfit: minProfitMap[TOKENS.WETH],
+            uniswapV3Fee: 3000,
+        },
+        {
+            tokenIn: TOKENS.WETH,
+            tokenOut: TOKENS.SNX,
+            amountIn: getRandomAmount(1, 10, 18),
+            minProfit: minProfitMap[TOKENS.WETH],
+            uniswapV3Fee: 3000,
+        },
+
+        // FIXME: OneInchSwapFail()
+        // {
+        //     tokenIn: TOKENS.wstETH,
+        //     tokenOut: TOKENS.WETH,
+        //     amountIn: getRandomAmount(1, 10, 18),
+        //     minProfit: minProfitMap[TOKENS.WETH],
+        //     uniswapV3Fee: 100,
+        // },
+    ]
+
+    return sampleSize(intentions, size)
+}
+
+export const oneInchProtocols = [
+    // "OPTIMISM_UNISWAP_V3",
+    // "OPTIMISM_SYNTHETIX",
+    // "OPTIMISM_SYNTHETIX_WRAPPER",
+    "OPTIMISM_ONE_INCH_LIMIT_ORDER",
+    "OPTIMISM_ONE_INCH_LIMIT_ORDER_V2",
+    "OPTIMISM_ONE_INCH_LIMIT_ORDER_V3",
+    // "OPTIMISM_CURVE",
+    "OPTIMISM_BALANCER_V2",
+    "OPTIMISM_VELODROME",
+    // "OPTIMISM_KYBERSWAP_ELASTIC",
+    // "OPTIMISM_CLIPPER_COVES",
+    // "OPTIMISM_KYBER_DMM_STATIC",
+    // "OPTIMISM_AAVE_V3",
+    // "OPTIMISM_ELK",
+    "OPTIMISM_WOOFI_V2",
+    // "OPTIMISM_TRIDENT",
+    "OPTIMISM_MUMMY_FINANCE",
+    // "OPTIMISM_NOMISWAPEPCS",
+    "OPTIMISM_VELODROME_V2",
+    // "OPTIMISM_WOMBATSWAP",
+]
