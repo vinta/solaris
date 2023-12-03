98cc8d9f3b12ae42d1a4d9ba159252e7fb18703b
Vinta Chen <vinta.chen@gmail.com>
2023-12-04 06:32:29 +0800

    fix

--- packages/flash-triangular-arbitrageur/src/configs.ts
+++ packages/flash-triangular-arbitrageur/src/configs.ts
@@ -86,18 +86,29 @@ export function getRandomIntentions(size: number) {
             minProfit: minProfitMap[TOKENS.USDCe],
             arbitrageFunc: ArbitrageFunc.UniswapV3SwapRouter,
         },
+        {
+            path: solidityPacked(
+                ["address", "uint24", "address", "uint24", "address", "uint24", "address"],
+                [TOKENS.USDC, 100, TOKENS.USDCe, 100, TOKENS.USDT, 100, TOKENS.USDC],
+            ),
+            tokens: [],
+            tokenIn: TOKENS.USDC,
+            amountIn: getRandomAmount(20000, 50000, 6),
+            minProfit: minProfitMap[TOKENS.USDC],
+            arbitrageFunc: ArbitrageFunc.UniswapV3SwapRouter,
+        },
+        {
+            path: solidityPacked(
+                ["address", "uint24", "address", "uint24", "address", "uint24", "address"],
+                [TOKENS.USDC, 100, TOKENS.USDT, 100, TOKENS.USDCe, 100, TOKENS.USDC],
+            ),
+            tokens: [],
+            tokenIn: TOKENS.USDC,
+            amountIn: getRandomAmount(20000, 50000, 6),
+            minProfit: minProfitMap[TOKENS.USDC],
+            arbitrageFunc: ArbitrageFunc.UniswapV3SwapRouter,
+        },
         // FIXME
-        // {
-        //     path: solidityPacked(
-        //         ["address", "uint24", "address", "uint24", "address", "uint24", "address"],
-        //         [TOKENS.USDCe, 100, TOKENS.USDC, 100, TOKENS.DAI, 100, TOKENS.USDCe],
-        //     ),
-        //     tokens: [],
-        //     tokenIn: TOKENS.USDCe,
-        //     amountIn: getRandomAmount(20000, 100000, 6),
-        //     minProfit: minProfitMap[TOKENS.USDCe],
-        //     arbitrageFunc: ArbitrageFunc.UniswapV3SwapRouter,
-        // },
         // {
         //     path: solidityPacked(
         //         ["address", "uint24", "address", "uint24", "address", "uint24", "address"],
