6bfbc9cdd99ce0a284ba6f0733ac8645a41ceada
Vinta Chen <vinta.chen@gmail.com>
2023-12-15 00:59:29 +0800

    const sellSpreadPercent = Big(1.5) // 1.5%

--- packages/trading-bot/src/arbitrageur.ts
+++ packages/trading-bot/src/arbitrageur.ts
@@ -43,7 +43,7 @@ class FlashArbitrageurOnOptimism extends BaseArbitrageur {
 
         const wethAmount = parseUnits("0.8", 18)
         const wethProfit = parseUnits("0.016", 18) // 36 USD
-        const sellSpreadPercent = Big(2) // 2%
+        const sellSpreadPercent = Big(1.5) // 1.5%
         const buySpreadPercent = -Big(0.5) // 0.5%
 
         console.log("start", {
