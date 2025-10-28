7f577d769890169b6c1418e7d1688bd9f889da86
Vinta Chen <vinta.chen@gmail.com>
2025-10-28 19:16:13 +0800

    clean up

--- contracts/tokens/interfaces/IWETH9.sol
+++ contracts/tokens/interfaces/IWETH9.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import { IERC20Metadata } from "openzeppelin-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol";
-
-/// @title Interface for WETH9
-interface IWETH9 is IERC20Metadata {
-    /// @notice Deposit ether to get wrapped ether
+interface IWETH {
     function deposit() external payable;
-
-    /// @notice Withdraw wrapped ether to get ether
     function withdraw(uint256) external;
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
 }
